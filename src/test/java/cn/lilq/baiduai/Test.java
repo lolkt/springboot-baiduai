7a47b0dcd4f6681c6d424cbbb112ecc5de753da7
lolkt <969070578@qq.com>
2020-01-17 17:07:22 +0800

    添加 人脸监测结果

--- src/test/java/cn/lilq/baiduai/Test.java
+++ src/test/java/cn/lilq/baiduai/Test.java
@@ -4,8 +4,10 @@ import cn.lilq.baiduai.util.Base64Util;
 import cn.lilq.baiduai.util.FileUtil;
 import cn.lilq.baiduai.util.GsonUtils;
 import com.baidu.aip.face.AipFace;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,42 +20,68 @@ public class Test {
         // 可选：设置网络连接参数
         aipFace.setConnectionTimeoutInMillis(2000);
         aipFace.setSocketTimeoutInMillis(60000);
-//        byte[] bytes = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\caiying\\1578534667097_80.jpg");
-//        String encode = Base64Util.encode(bytes);
-//        JSONObject jsonObject = aipFace.addUser(encode, "BASE64", "girls", "1", null);
-//        System.out.println("===" + jsonObject.toString());
 
-//        byte[] bytes2 = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\caiying\\1578534672083_82.jpg");
-//        String encode2 = Base64Util.encode(bytes2);
-//        JSONObject jsonObject2 = aipFace.addUser(encode2, "BASE64", "girls", "1", null);
-//        System.out.println("===" + jsonObject2.toString());
-//        if ("SUCCESS".equals(jsonObject2.optString("error_msg"))) {
-//            AddUserReply userReply = GsonUtils.fromJson(jsonObject2.toString(), AddUserReply.class);
-//            System.out.println("===" + userReply.getLog_id());
-//        }
+//        addUser(aipFace);
+//
+//        addUser2(aipFace);
+//
+//        search(aipFace);
+
+        detect(aipFace);
+    }
+
+    public static void detect(AipFace client) throws IOException, JSONException {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("face_field", "age,gender,face_type,beauty");
+        options.put("max_face_num", "2");
+        options.put("face_type", "LIVE");
+        options.put("liveness_control", "NORMAL");
+
+        byte[] bytesSearch = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\reba\\timg2.jpg");
+        String image = Base64Util.encode(bytesSearch);
+        String imageType = "BASE64";
+
+        // 人脸检测
+        JSONObject res = client.detect(image, imageType, options);
+        System.out.println(res.toString(2));
+
+    }
 
 
+    private static void addUser2(AipFace aipFace) throws IOException {
+        byte[] bytes2 = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\caiying\\1578534672083_82.jpg");
+        String encode2 = Base64Util.encode(bytes2);
+        JSONObject jsonObject2 = aipFace.addUser(encode2, "BASE64", "girls", "1", null);
+        System.out.println("===" + jsonObject2.toString());
+        if ("SUCCESS".equals(jsonObject2.optString("error_msg"))) {
+            AddUserReply userReply = GsonUtils.fromJson(jsonObject2.toString(), AddUserReply.class);
+            System.out.println("===" + userReply.getLog_id());
+        }
+    }
+
+    private static void search(AipFace aipFace) throws IOException {
         byte[] bytesSearch = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\reba\\timg2.jpg");
         String encodeSearch = Base64Util.encode(bytesSearch);
         System.out.println(encodeSearch);
-         HashMap<String, String> options=new HashMap<>();
-         /**
-         * 质量控制(质量不符合要求的人脸不会出现在返回结果中)
-         * NONE: 不进行控制
-         * LOW:较低的质量要求
-         * NORMAL: 一般的质量要求
-         * HIGH: 较高的质量要求
-         * 默认NONE
-         *//*
-        options.put("quality_control","NONE");
-         /**
-         * 活体控制(活体分数不符合要求的人脸不会出现在返回结果中)
-         * NONE: 不进行控制
-         * LOW:较低的活体要求(高通过率 低攻击拒绝率)
-         * NORMAL: 一般的活体要求(平衡的攻击拒绝率, 通过率)
-         * HIGH: 较高的活体要求(高攻击拒绝率 低通过率)
-         * 默认NONE
-         */
+        HashMap<String, String> options=new HashMap<>();
+        /**
+        * 质量控制(质量不符合要求的人脸不会出现在返回结果中)
+        * NONE: 不进行控制
+        * LOW:较低的质量要求
+        * NORMAL: 一般的质量要求
+        * HIGH: 较高的质量要求
+        * 默认NONE
+        *//*
+       options.put("quality_control","NONE");
+        /**
+        * 活体控制(活体分数不符合要求的人脸不会出现在返回结果中)
+        * NONE: 不进行控制
+        * LOW:较低的活体要求(高通过率 低攻击拒绝率)
+        * NORMAL: 一般的活体要求(平衡的攻击拒绝率, 通过率)
+        * HIGH: 较高的活体要求(高攻击拒绝率 低通过率)
+        * 默认NONE
+        */
         options.put("liveness_control","NONE");
         JSONObject jsonSearch = aipFace.search(encodeSearch, "BASE64", "girls", options);
         System.out.println("===" + jsonSearch.toString());
@@ -72,5 +100,13 @@ public class Test {
             }
 
         }
+    }
+
+    private static void addUser(AipFace aipFace) throws IOException {
+
+        byte[] bytes = FileUtil.readFileByBytes("E:\\work2019\\java-sdk\\src\\webapp\\caiying\\1578534667097_80.jpg");
+        String encode = Base64Util.encode(bytes);
+        JSONObject jsonObject = aipFace.addUser(encode, "BASE64", "girls", "1", null);
+        System.out.println("===" + jsonObject.toString());
     }
 }
